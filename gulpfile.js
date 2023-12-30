2e1148d26b3ced2db045af74c1fa5f4567f18956
vEnCa-X <vencax@gmail.com>
2023-12-30 09:10:45 +0100

    zakladni workflows

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ gulp.task('shell_composer_update', shell.task('composer update'));
 gulp.task('shell_composer_update_prefer_lowest', shell.task('composer update --no-progress --prefer-dist --prefer-lowest --prefer-stable'));
 gulp.task('shell_test', shell.task('vendor\\bin\\tester tests -s -p php'));
 
-gulp.task('shell_netteCodeChecker', shell.task('php ..\\..\\nette-code-checker\\code-checker -d src --short-arrays --strict-types'));
-gulp.task('shell_netteCodeCheckerFIX', shell.task('php ..\\..\\nette-code-checker\\code-checker -d src --short-arrays --strict-types --fix'));
+gulp.task('shell_netteCodeChecker', shell.task('php ..\\..\\nette-code-checker\\code-checker -d src --strict-types --no-progress --ignore expected'));
+gulp.task('shell_netteCodeCheckerFIX', shell.task('php ..\\..\\nette-code-checker\\code-checker -d src --strict-types --no-progress --ignore expected --fix'));
 gulp.task('shell_netteCodingStandard', shell.task('php ..\\..\\nette-coding-standard\\ecs check src tests --preset php81'));
 gulp.task('shell_netteCodingStandardFIX', shell.task('php ..\\..\\nette-coding-standard\\ecs check src tests --preset php81 --fix'));
 
